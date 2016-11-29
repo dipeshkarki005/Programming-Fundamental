cffac0ed50b2a15327442dc81fa01c6188dff7da
dipeshkarki005 <dipesh.karki005@gmail.com>
2016-11-29 19:20:01 +0545

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,35 @@
+/*
+Student Name : Dipesh Karki
+Subject : Programming fundamental
+Program : write a c program to enter any number and calculate its square
+lab sheet : 07
+Date : 28 November , 2016
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a;
+printf("Enter the number:\n");
+
+scanf("%d",&a);
+
+if(a%2==0){
+
+printf("\nThe number is even");
+
+}
+
+else
+
+{
+printf("\nThe number is odd");
+
+}
+
+return 0;
+
+getch();
+
+}
