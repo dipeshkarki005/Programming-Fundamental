d1770db3834e4201cfdcca2ff4172c211a89c78e
dipeshkarki005 <dipesh.karki005@gmail.com>
2016-12-07 10:35:02 +0545

    Add files via upload

---
+++ 213213.c
@@ -0,0 +1,38 @@
+/*
+Student Name : Dipesh Karki
+Roll No.: 13
+Subject : Programming fundamental
+Program : write a c program to
+lab sheet : 10
+Date : 5 December , 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,c;
+printf("\nEnter the first number (a): ");
+scanf("%d",&a);
+
+printf("\nEnter the second number (b): ");
+scanf("%d",&b);
+
+printf("\nEnter the third number (c): ");
+scanf("%d",&c);
+
+if(a>b && a>c)
+{
+    printf("\nThe greater number is a: %d",a);
+}
+else if(b>a && b>c)
+{
+    printf("\nThe greater number is b: %d",b);
+}
+else
+{
+    printf("\nThe greater number is c: %d",c);
+}
+return 0;
+
+getch ();
+}
