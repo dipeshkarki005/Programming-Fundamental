81fc6425039a3bad899babc2c4670d6750ebfbab
dipeshkarki005 <dipesh.karki005@gmail.com>
2016-12-09 10:36:27 +0545

    Add files via upload

---
+++ ternary operator.c
@@ -0,0 +1,19 @@
+/*
+Student Name : Dipesh Karki
+Roll No.: 13
+Subject : Programming fundamental
+Program : write a c program to use ternary operator.
+lab sheet : 10
+Date : 7 December , 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int n;
+printf("Enter the number: ");
+scanf("%d",&n);
+(n%2==0)? printf("\n%d is even",n) : printf("\n%d is odd",n);
+return 0;
+getch ();
+
+}
